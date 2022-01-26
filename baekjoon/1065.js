8325aa09aa6d36e9246acb033e705ad6aa859f80
Inhwa Lee <54103723+014ee@users.noreply.github.com>
2022-01-26 15:34:01 +0900

    함수: 한수

--- baekjoon/1065.js
+++ baekjoon/1065.js
@@ -1,21 +1,21 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split('\n');
-var a = parseInt(input[0]);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+input = Number(input);
 
 function solution(N) {
   const hansu = new Array(99).fill(0)
   let result
   if(N < 100) result = N;  // 100 이하는 모두 한수
   
-  for(let i = 100; i <= N; i++){ // 100~N사이의 숫자
-    let splitArr = String(i).split('')
+  for(let num = 100; num <= N; num++){ // 100~N 사이의 숫자
+    let splitNum = String(num).split('')
     let diff = new Set();
     
-    for(let j = 1; j < splitArr.length; j++){
-      diff.add(splitArr[j - 1] - splitArr[j])
+    for(let i = 1; i < splitNum.length; i++){
+      diff.add(splitNum[i - 1] - splitNum[i])
     }
     if(diff.size === 1){
-      hansu.push(i)
+      hansu.push(num)
     }
     result = hansu.length
   }
@@ -23,4 +23,4 @@ function solution(N) {
   console.log(result)
 }
 
-solution(a)
+solution(input)
