cd75f31838f28369ee2f277549e6e9935432decf
Inhwa Lee <54103723+014ee@users.noreply.github.com>
2022-01-26 15:08:37 +0900

    함수: 셀프 넘버 (컨닝했으므로 추후 다시 풀어보기)

--- baekjoon/4673.js
+++ baekjoon/4673.js
@@ -2,13 +2,12 @@ let array = []
 
 function solution (num){
   let sum = num
-  
+ 
   while(1){
     if(num === 0) break;
     sum += num % 10; //주어진 정수를 뒤에서부터 나머지로 전환 후 더하기
     num = parseInt(num / 10); // 더한 숫자는 제외
   }
-  
   array[sum] = sum; // 더해진 숫자의 배열에 넣기 (1 ==> 1번째 배열)
   return sum
 }
